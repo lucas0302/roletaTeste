60596805fa0390b321cf78b89f50ecc4f66e0489
Lucas Araujo <lucasao744@gmail.com>
2024-02-25 15:49:33 -0300

    const

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React, { useRef, useState } from 'react';
 import './css/App.css';
-import pedroImage from './images/pedro.jpg';
-import lucasImage from './images/lucas.jpg';
-import hugoImage from './images/hugo.jpg';
-import anaImage from './images/ana.jpg';
-import antonioImage from './images/antonio.jpg';
-import juliaImage from './images/julia.jpg';
-import marcosImage from './images/marcos.jpg';
-import mariaImage from './images/maria.jpg';
-import victorImage from './images/victor.jpg';
-
-const items = [
-  { id: 1, name: 'Pedro', img: pedroImage },
-  { id: 2, name: 'Lucas', img: lucasImage },
-  { id: 3, name: 'Hugo', img: hugoImage },
-  { id: 4, name: 'Ana', img: anaImage },
-  { id: 5, name: 'Antônio', img: antonioImage },
-  { id: 6, name: 'Julia', img: juliaImage },
-  { id: 7, name: 'Marcos', img: marcosImage },
-  { id: 8, name: 'Maria', img: mariaImage },
-  { id: 9, name: 'Victor', img: victorImage },
-
-];
+import { items } from './const';
 
 const duplicatedItems = [...items, ...items];
-
 const App = () => {
   const rouletteRef = useRef(null);
   const [selectedItem, setSelectedItem] = useState(null);
---
+++ src/const.js
@@ -0,0 +1,22 @@
+import pedroImage from './images/pedro.jpg';
+import lucasImage from './images/lucas.jpg';
+import hugoImage from './images/hugo.jpg';
+import anaImage from './images/ana.jpg';
+import antonioImage from './images/antonio.jpg';
+import juliaImage from './images/julia.jpg';
+import marcosImage from './images/marcos.jpg';
+import mariaImage from './images/maria.jpg';
+import victorImage from './images/victor.jpg';
+
+export const items = [
+    { id: 1, name: 'Pedro', img: pedroImage },
+    { id: 2, name: 'Lucas', img: lucasImage },
+    { id: 3, name: 'Hugo', img: hugoImage },
+    { id: 4, name: 'Ana', img: anaImage },
+    { id: 5, name: 'Antônio', img: antonioImage },
+    { id: 6, name: 'Julia', img: juliaImage },
+    { id: 7, name: 'Marcos', img: marcosImage },
+    { id: 8, name: 'Maria', img: mariaImage },
+    { id: 9, name: 'Victor', img: victorImage },
+  
+  ];
